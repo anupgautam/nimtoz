3abb70c4c11dfd9741ae41f3d4e96df2a3280c1d
lemongautam79 <lemongautam79@gmail.com>
2024-10-18 10:41:53 +0545

    [Build successfull]

--- src/components/Footer/Chatfooter.tsx
+++ src/components/Footer/Chatfooter.tsx
@@ -56,7 +56,7 @@ const ChatFooter = () => {
                                         </a>
                                     </li>
                                     <li>
-                                        <a target="_blank" className="flex items-center text-gray-700 transition hover:text-[#ff5a1f]" href="mailto:bookvenuenepal@gmail.com">
+                                        <a target="_blank" className="flex items-center text-gray-700 transition hover:text-[#ff5a1f]" href="mailto:nimtozsewa@gmail.com">
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                                                 <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
                                                 <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
